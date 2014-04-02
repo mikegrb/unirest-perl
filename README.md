cb50f5efd2e86428962691c070b69f61168e1bc9
mikegrb <mgreb@linode.com>
2014-04-02 19:04:14 -0400

    initial template

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+Unirest - Blah blah blah
+
+# SYNOPSIS
+
+    use Unirest;
+
+# DESCRIPTION
+
+Unirest is
+
+# AUTHOR
+
+Mike Greb <michael@thegrebs.com>
+
+# COPYRIGHT
+
+Copyright 2014- Mike Greb
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/Unirest.pm
@@ -0,0 +1,39 @@
+package Unirest;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Unirest - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Unirest;
+
+=head1 DESCRIPTION
+
+Unirest is
+
+=head1 AUTHOR
+
+Mike Greb E<lt>michael@thegrebs.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2014- Mike Greb
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
